fff4a74297a1da4dd5ab2eac29e33729d72bc622
chetan6780 <crp20802@gmail.com>
2021-07-04 18:13:46 +0530

    stack using queue

--- GFG_other/stack/StackFunctionsUserDefStack.cpp
+++ GFG_other/stack/StackFunctionsUserDefStack.cpp
@@ -87,3 +87,126 @@ int evaluatePostfix(char *exp)
     }
     return pop(stack);
 }
+
+// Implimentation of stack using Queue ----------------------------------------------------------------
+// Method 1 (By making push operation costly)
+
+// class Stack
+// {
+//     queue<int> q1, q2;
+//     int curr_size;
+
+// public:
+//     Stack()
+//     {
+//         curr_size = 0;
+//     }
+
+//     void push(int x)
+//     {
+//         curr_size++;
+
+//         q2.push(x);
+
+//         while (!q1.empty())
+//         {
+//             q2.push(q1.front());
+//             q1.pop();
+//         }
+
+//         // swap the names of two queues
+//         queue<int> q = q1;
+//         q1 = q2;
+//         q2 = q;
+//     }
+
+//     void pop()
+//     {
+
+//         if (q1.empty())
+//             return;
+//         q1.pop();
+//         curr_size--;
+//     }
+
+//     int top()
+//     {
+//         if (q1.empty())
+//             return -1;
+//         return q1.front();
+//     }
+
+//     int size()
+//     {
+//         return curr_size;
+//     }
+// };
+
+// Method 2 (By making pop operation costly)
+
+// class Stack
+// {
+//     queue<int> q1, q2;
+//     int curr_size;
+
+// public:
+//     Stack()
+//     {
+//         curr_size = 0;
+//     }
+
+//     void pop()
+//     {
+//         if (q1.empty())
+//             return;
+
+//         while (q1.size() != 1)
+//         {
+//             q2.push(q1.front());
+//             q1.pop();
+//         }
+
+//         q1.pop();
+//         curr_size--;
+
+//         // swap the names of two queues
+//         queue<int> q = q1;
+//         q1 = q2;
+//         q2 = q;
+//     }
+
+//     void push(int x)
+//     {
+//         q1.push(x);
+//         curr_size++;
+//     }
+
+//     int top()
+//     {
+//         if (q1.empty())
+//             return -1;
+
+//         while (q1.size() != 1)
+//         {
+//             q2.push(q1.front());
+//             q1.pop();
+//         }
+
+//         int temp = q1.front();
+
+//         q1.pop();
+
+//         q2.push(temp);
+
+//         // swap the two queues names
+//         queue<int> q = q1;
+//         q1 = q2;
+//         q2 = q;
+//         return temp;
+//     }
+
+//     int size()
+//     {
+//         return curr_size;
+//     }
+// };
